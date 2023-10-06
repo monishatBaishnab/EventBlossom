f3f295fc2c56e0224f41184c86ae5a2d555f501a
monishat <monishat@gmail.com>
2023-10-07 01:28:08 +0600

    Create Footer Component.

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,9 +1,27 @@
+import { Link, NavLink } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer>
+            <div className="border-b">
+                <div className="c-container">
+                    <div className="mb-5">
+                        <Link className="text-3xl text-center block text-green-400 font-medium">EventBlossom</Link>
+                    </div>
+                    <div>
+                        <ul className="flex gap-3 flex-col items-center justify-center sm:flex-row">
+                            <li><NavLink className='text-gray-500 transition-all hover:text-green-500' to='/'>Home</NavLink></li>
+                            <li><NavLink className='text-gray-500 transition-all hover:text-green-500' to='/about'>About</NavLink></li>
+                            <li><NavLink className='text-gray-500 transition-all hover:text-green-500' to='/events'>Events</NavLink></li>
+                            <li><NavLink className='text-gray-500 transition-all hover:text-green-500' to='/vanueList'>Vanue List</NavLink></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="c-container py-5">
+                <p className="text-center text-gray-700">&copy; 2023 <Link to='/' className="text-green-500">EventBlossom</Link>. All rights reserved.</p>
+            </div>
+        </footer>
     );
 };
 
